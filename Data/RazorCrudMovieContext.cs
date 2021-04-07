22f2e225d1a0d9104751e1dc55f736b31b9ac855
Peter <dhondt.peter@gmail.com>
2021-04-07 11:59:12 +0200

    generated CRUD from template razorpage

---
+++ Data/RazorCrudMovieContext.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorCrud.Models;
+
+    public class RazorCrudMovieContext : DbContext
+    {
+        public RazorCrudMovieContext (DbContextOptions<RazorCrudMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorCrud.Models.Movie> Movie { get; set; }
+    }
--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
 
 namespace RazorCrud
 {
@@ -24,6 +25,9 @@ namespace RazorCrud
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+
+            services.AddDbContext<RazorCrudMovieContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("RazorCrudMovieContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
